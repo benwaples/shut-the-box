149f6aef523b212c4f6d790aaf770b5255c6e1c5
bwapes <benwaples@gmail.com>
2021-02-11 14:50:13 -0800

    change diceSum to be array of numbers and add restart button

--- src/components/shutTheBox/ShutTheBox.tsx
+++ src/components/shutTheBox/ShutTheBox.tsx
@@ -5,11 +5,15 @@ import DisplayNumbers from '../displayNumber/DisplayNumbers';
 const initialGame = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function ShutTheBox(): JSX.Element {
-  const [diceSum, setDiceSum] = useState<number>();
+  const [diceSum, setDiceSum] = useState<number[]>([0, 0]);
   const [remainingNumbers, setRemainingNumbers] = useState<number[]>(
     initialGame
   );
 
+  function handleRestart() {
+    setDiceSum([1, 1]);
+    setRemainingNumbers(initialGame);
+  }
   return (
     <>
       <header>
@@ -21,12 +25,22 @@ export default function ShutTheBox(): JSX.Element {
           <DisplayNumbers {...{ remainingNumbers, setRemainingNumbers }} />
         </section>
         <section>
-          <h3>{diceSum}</h3>
+          <h3>
+            {diceSum[0]}, {diceSum[1]}
+          </h3>
           <button
-            onClick={() => setDiceSum(Math.ceil(Math.random() * 6))}
+            onClick={() =>
+              setDiceSum([
+                Math.ceil(Math.random() * 6),
+                Math.ceil(Math.random() * 6),
+              ])
+            }
             type="button"
           >
             Roll Dice
+          </button>
+          <button type="button" onClick={handleRestart}>
+            Restart
           </button>
         </section>
       </main>
