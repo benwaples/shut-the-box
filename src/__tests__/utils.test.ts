85fbbdc12a9e06548649046f4a72647557d76cbe
benwaples <benwaples@gmail.com>
2021-02-24 11:14:07 -0800

    removeBlocks()

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,7 +1,30 @@
+import { removeBlocks } from '../utils';
+
 export {};
 
 describe('util functions', () => {
   it('removeBlocks(array: RemainingBlocks[])', () => {
-    expect(true).toEqual(true);
+    const exampleData = [
+      {
+        number: 1,
+        isPlayed: false,
+      },
+      {
+        number: 2,
+        isPlayed: false,
+      },
+      {
+        number: 3,
+        isPlayed: false,
+      },
+      {
+        number: 4,
+        isPlayed: true,
+      },
+    ];
+    const expected = exampleData.slice();
+    const actual = removeBlocks(exampleData);
+    expected.pop();
+    expect(actual).toEqual(expected);
   });
 });
